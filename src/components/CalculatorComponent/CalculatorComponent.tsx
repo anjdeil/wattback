e5d65c1fca562adb62a8e8f0c61e43d828501687
DigiwaymediaDenys <denys@digiwaymedia.com>
2025-03-11 13:18:22 +0100

    setup

---
+++ src/components/CalculatorComponent/CalculatorComponent.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './style.scss';
+
+const CalculatorComponent: FC = () => {
+  return (
+    <div>CalculatorComponent</div>
+  )
+}
+
+export default CalculatorComponent;
---
+++ src/pages/CalculatorPage.tsx
@@ -0,0 +1,12 @@
+import CalculatorComponent from "../components/CalculatorComponent/CalculatorComponent";
+
+const CalculatorPage = () => {
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <CalculatorComponent />
+    </div>
+  );
+};
+
+export default CalculatorPage;
