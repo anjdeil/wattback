4c488bd2d1c53fdc736a4186788b8716bdbc00dc
DigiwaymediaDenys <denys@digiwaymedia.com>
2025-03-14 13:44:57 +0100

    remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import CalculatorPage from './pages/CalculatorPage';
 import './styles/main.scss';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Wattback Home Page</h1>} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-      </Routes>
-    </Router>
+    <CalculatorPage />
   )
 }
 
