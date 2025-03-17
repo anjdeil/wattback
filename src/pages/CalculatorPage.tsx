1022679891ff44d27ee1689fa7283c5525182c2c
camelCaserJs <snake_case@i.ua>
2025-03-17 14:09:28 +0100

    Change to test webhook

--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -6,7 +6,7 @@ const CalculatorPage = () => {
         <div className="calculator container">
             <div className="calculator__block calculator__block--top">
                 <h1>Solar power plant calculator – cost and benefit calculation.</h1>
-                <p>How much does a solar system cost? When will it pay off? Our calculator will answer these questions and show you how to invest in energy independence with maximum benefit.</p>
+                <p>How much does a solar system cost? When will it pay off? Our calculator will answer these questions and show you how to invest in energy independence with maximum benefit!</p>
             </div>
             <div className="calculator__container">
                 <CalculatorComponent />
